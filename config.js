ce8ef4eda2bd2ccdc80cb966d37e2f8e3aef9133
Seniru938 <bsedesilva21@gmail.com>
2026-01-30 11:50:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
-    // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    // Example: "Jack", "Brian", "Bryce"
+    valentineName: "Caitlyn",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -25,7 +25,7 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+         
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
